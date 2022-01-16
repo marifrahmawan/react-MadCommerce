7e9c120de7e44abf38950f2de40f36fefee02f98
marifrahmawan <m1600018181@webmail.uad.ac.id>
2022-01-16 22:04:14 +0800

    adding category component

--- src/data.js
+++ src/data.js
@@ -21,3 +21,41 @@ export const sliderItems = [
     bg: 'ffe599',
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1602810316693-3667c854239a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    title: 'SHIRT',
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+    title: 'JACKET',
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1620554740172-7c02a78b239d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    title: 'SHOES',
+  },
+  {
+    id: 4,
+    img: 'https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+    title: 'LAPTOP',
+  },
+  {
+    id: 5,
+    img: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+    title: 'SMARTPHONE',
+  },
+  {
+    id: 5,
+    img: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+    title: 'SMARTPHONE',
+  },
+  {
+    id: 5,
+    img: 'https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+    title: 'SMARTPHONE',
+  },
+];
